8e4fbf0dc4a09b9837d168c59103fb6f7e3a845f
Rad Suchecki <r.suchecki@gmail.com>
2019-05-20 16:13:16 +1000

    rplots

--- bin/plot_simulatedDNA.R
+++ bin/plot_simulatedDNA.R
@@ -22,9 +22,12 @@ res2 <- melt(res, na.rm = TRUE, id.vars = c("meta.tool",
 
 ggplot(na.omit(res2), aes(x=meta.tool, y=as.numeric(value), fill=variable)) +
   scale_y_continuous()+
+  labs(y = 'number of reads', x = 'tool') +
   geom_bar(stat="identity",position = position_stack(reverse = TRUE)) +
   coord_flip() +
-  theme(legend.position = "top") +
+  theme(legend.position = "top", legend.title=element_blank()) +
+  scale_fill_discrete(breaks=c("results.M_1", "results.M_2", "results.w","results.u"),
+                        labels=c("R1 correct", "R2 correct", "Wrong", "Unaligned"))+
   facet_grid(meta.simulator~meta.mode~meta.species)
 
 ggsave(file="simulatedDNA_summaries.pdf", width=16, height=9);
