65ccb7e594a9051ed3be5efe34c688cc7449d44b
Radoslaw Suchecki <r.suchecki@gmail.com>
2019-06-13 16:22:29 +0930

    removed template

--- templates/rna2dna/biokanga_align.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-biokanga align \
-  --sfx ${idxmeta.target}.sfx \
-  --in ${reads[0]} \
-  --pair ${reads[1]}  \
-  --out out.bam \
-  --threads ${task.cpus} \
-  ${ALIGN_PARAMS}
