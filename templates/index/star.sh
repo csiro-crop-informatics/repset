e18b4eb887fc7cc0ccedaeb2bdbd6513404ee2cf
Rad Suchecki <r.suchecki@gmail.com>
2020-07-17 14:49:41 +0930

    Generalised fix for stray STAR log file

--- templates/index/star.sh
+++ templates/index/star.sh
@@ -5,4 +5,6 @@ STAR --runThreadN ${task.cpus} \
   --runMode genomeGenerate \
   --genomeDir genomeDir \
   --genomeFastaFiles ${ref}
-mv Log.out indexing_Log.out #otherwise file overwritten by align process which in turn triggers re-runs of align due to cache mismatch
+
+#fixed and no longer needed in STAR approx > 2.7.0f <= 2.7.5a . Previously file overwritten by align process which in turn triggers re-runs of align process due to cache mismatch
+[ -f Log.out ] && mv Log.out indexing_Log.out || echo "no Log.out to cleanup"
